ddb0b2aead2c6551f14e87d5359ea58a89881482
azam alias <azamalias@gmail.com>
2018-01-06 11:27:12 +0800

    added generated step definitions from previous cucumber run output

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,12 @@
+Given(/^there is a User$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+Given(/^the User has posted the message "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+When(/^I visit the page for the User$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+Then(/^I should see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
