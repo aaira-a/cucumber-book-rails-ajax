55c6da7b93809583cb8e362eb3b36c8099b57048
azam alias <azamalias@gmail.com>
2018-01-06 13:29:49 +0800

    implemented second step

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,8 +1,9 @@
 Given(/^there is a User$/) do
   FactoryGirl.create(:user)
 end
-Given(/^the User has posted the message "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^the User has posted the message "([^"]*)"$/) do |message_text|
+  expect(User.count).to eq 1
+  FactoryGirl.create(:message, :content => message_text, :user => User.first)
 end
 When(/^I visit the page for the User$/) do
   pending # Write code here that turns the phrase above into concrete actions
